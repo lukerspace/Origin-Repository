d2af3c765f4d79843a54fc47c1746566539a4814
lukerspace.hsieh <lukerspace@gmail.com>
2021-05-01 19:06:22 +0800

    Add index.js & modify css

--- app.py
+++ app.py
@@ -1,23 +1,19 @@
+# from config import Config
 from flask import *
 import json
 import mysql.connector 
-from flask import *
 from sql import cursor, conn , mysql_select
 from getpass import getpass
-# from config import Config
+from config import Config
+from api.attraction_api import attraction_api
+
 
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 app.config["JSON_SORT_KEYS"] = False
 
-conn=mysql.connector.connect(
-		host = "localhost",
-		user=input("Enter username: "),
-		password=getpass("Enter password: "),
-		database = "taipei",charset = "utf8",
-		auth_plugin='mysql_native_password')
-cursor=conn.cursor()
+app.register_blueprint(attraction_api, url_prefix='/api')
 
 # Pages
 @app.route("/")
@@ -34,45 +30,45 @@ def thankyou():
 	return render_template("thankyou.html")
 
 
-@app.route('/api/attractions')
-def attractions():
-	if request.args.get('page'):
-		page = request.args.get('page')
-		page=int(page)
-		index = page * 12
-		next_page_JSON = page + 1
-		if request.args.get('keyword'):
-			keyword =request.args.get('keyword')
-			attraction_JSON = mysql_select(f"SELECT * FROM attraction WHERE name LIKE '%{keyword}%' LIMIT {index},12")
-			next_list= mysql_select(f"SELECT * FROM attraction WHERE name LIKE '%{keyword}%' LIMIT {index + 12}, 12")
-			if len(next_list) == 0:
-				next_page_JSON = None
-			data = {"nextPage": next_page_JSON,"data": attraction_JSON}
-			return jsonify(data)
-		else:
-			attraction_JSON = mysql_select(f"SELECT * FROM attraction LIMIT {index}, 12")
-			next_list = mysql_select(f"SELECT * FROM attraction LIMIT {index + 12}, 12")
-			print("目前頁面:",page)
-			if index+1<320:
-				print("起始ID:",index+1)
-			if len(next_list) == 0:
-				next_page_JSON = None
-			data = {"nextPage": next_page_JSON,"data": attraction_JSON}
-			return jsonify(data)
-	return {"error": True,"message": "伺服器內部錯誤"}, 500
+# @app.route('/api/attractions')
+# def attractions():
+# 	if request.args.get('page'):
+# 		page = request.args.get('page')
+# 		page=int(page)
+# 		index = page * 12
+# 		next_page_JSON = page + 1
+# 		if request.args.get('keyword'):
+# 			keyword =request.args.get('keyword')
+# 			attraction_JSON = mysql_select(f"SELECT * FROM attraction WHERE name LIKE '%{keyword}%' LIMIT {index},12")
+# 			next_list= mysql_select(f"SELECT * FROM attraction WHERE name LIKE '%{keyword}%' LIMIT {index + 12}, 12")
+# 			if len(next_list) == 0:
+# 				next_page_JSON = None
+# 			data = {"nextPage": next_page_JSON,"data": attraction_JSON}
+# 			return jsonify(data)
+# 		else:
+# 			attraction_JSON = mysql_select(f"SELECT * FROM attraction LIMIT {index}, 12")
+# 			next_list = mysql_select(f"SELECT * FROM attraction LIMIT {index + 12}, 12")
+# 			print("目前頁面:",page)
+# 			if index+1<320:
+# 				print("起始ID:",index+1)
+# 			if len(next_list) == 0:
+# 				next_page_JSON = None
+# 			data = {"nextPage": next_page_JSON,"data": attraction_JSON}
+# 			return jsonify(data)
+# 	return {"error": True,"message": "伺服器內部錯誤"}, 500
 
-@app.route('/api/attraction/<int:ID>')
-def api_attraction(ID):
-	if ID:
-		cursor.execute(f"SELECT * FROM attraction where id={ID}")
-		data = cursor.fetchone()
-		if data:
-			attraction = {"data": dict(zip(cursor.column_names, data))}
-			attraction['data']['images'] = json.loads(data[9])
-			return attraction
-		return jsonify({ "error": True, "message": "景點編號不正確" })
+# @app.route('/api/attraction/<int:ID>')
+# def api_attraction(ID):
+# 	if ID:
+# 		cursor.execute(f"SELECT * FROM attraction where id={ID}")
+# 		data = cursor.fetchone()
+# 		if data:
+# 			attraction = {"data": dict(zip(cursor.column_names, data))}
+# 			attraction['data']['images'] = json.loads(data[9])
+# 			return attraction
+# 		return jsonify({ "error": True, "message": "景點編號不正確" })
 	
-	return jsonify({ "error": True, "message": "伺服器內部錯誤" })
+# 	return jsonify({ "error": True, "message": "伺服器內部錯誤" })
 
 
 app.run(host="0.0.0.0",port=3000)
---
+++ static/index.js
@@ -0,0 +1,188 @@
+let Main = document.getElementById('mains');
+//取得main的主要頁面
+let ip = "127.0.0.1"
+let nextPage = 0;
+let section;
+let keyword = null;
+
+//5. 將每個參數的欄位狀況，設計進去html的架構之中
+//<article>
+//  <a>整個api連結內容
+//      <img>圖片api中取得圖片
+//      <div>整個文字方塊    
+//          <div>上層簡介
+//              <p>捷運站
+//              <p>旅遊類別
+//          </div>
+//          <p>下層景點標題
+//      </div> 
+//  </a>
+//將page_list的json內容寫入attr_url超連結每個區塊之中。
+//再將整體超連結{{標籤為a (attr_url)}} 丟進box中，box為整個attraction方塊
+//回傳box
+
+function CreateItems(attraction){
+
+    let box = document.createElement('article');
+    box.classList.add('itembox');
+
+    let attr_url = document.createElement('a');
+    attr_url.href = `http://${ip}:3000/api/attraction/${attraction.id}`
+    attr_url.setAttribute('target', '_blank');
+    
+    let attr_img = document.createElement('img');
+    attr_img.src = attraction.images[0]
+    attr_img.addEventListener('load',CheckPoint);
+
+    //6. 製作attractionbox載入資料及照片時，每12個須紀錄換頁動作
+    //執行CheckPoint函數，完成12個attraction寫入後，將load_next改成true載入下一頁
+    //並且透過load_next的變化流程控制，下12個資料的寫入
+    
+    //{{7-1}}關鍵字流程如上
+    let attr_text = document.createElement('div');
+    attr_text.classList.add('textbox');
+    
+    let attr_title = document.createElement('p');
+    attr_title.classList.add('title');
+    attr_title.textContent = attraction.name;
+    
+    let attr_info = document.createElement('div');
+    attr_info.classList.add('info');
+    
+    let attr_mrt = document.createElement('p');
+    attr_mrt.classList.add('attraction-mrt');
+    attr_mrt.textContent = attraction.mrt;
+    
+    let attr_type = document.createElement('p');
+    attr_type.classList.add('attraction-category');
+    attr_type.textContent = attraction.category;
+   
+    attr_info.appendChild(attr_mrt);
+    attr_info.appendChild(attr_type);
+    
+    attr_text.appendChild(attr_info);
+    attr_text.appendChild(attr_title);
+    
+    attr_url.appendChild(attr_img);
+    attr_url.appendChild(attr_text);
+
+    
+    box.appendChild(attr_url);
+    console.log(box);
+    
+    return box;
+}
+// load and visualize attraction calling GetData and ShowData function
+// 2.利用LoadData從api取得資料串列。
+// 若是沒有關鍵字，即是查找keyword= null。並且若是有下一頁，將nextPage更新至下一頁。
+// 並呼叫ShowData() 函數。
+
+// 4-1 {{關鍵字設定}}
+// 透過input的keyword及函數預設的nextpage=0匯入GetPata(0,keyword)
+async function LoadData(keyword=null){
+    if(nextPage !== null){
+        nextPage = await GetData(nextPage,keyword);
+        ShowData();
+    }
+}
+
+// 3.將我的預設=0的nextPage導入GetData(pageNumber)中 {{此處函數output會更新nextPage}}
+// 並且此函數目的為取得page_list的頁面arry資料及nextPage的更新。
+
+async function GetData(pageNumber, keyword=null){
+    let url;
+    if(keyword){
+        url = `http://${ip}:3000/api/attractions?page=${pageNumber}&keyword=${keyword}`;
+    //5-1 得到資料API中的KEYWORD資料
+    }else{
+        url = `http://${ip}:3000/api/attractions?page=${pageNumber}`;
+    }
+    let response = await fetch(url);
+    let data = await response.json();
+    nextPage = data.nextPage;
+    page_list = data.data;
+    // console.log(page_list)
+    console.log("next page:",nextPage)
+    return nextPage;
+}
+// 4. 透過showdata確認本頁資料個數，並且產生出attractionbox
+
+// 將本頁面資料長度確認，並將本頁面，每一個{{單一資料}} LOAD 進我的CreateItems函數中 
+// 並且將我的整體attractionBox丟進 INDEX.HTML中的預設區域。
+// 預設initiator=0
+
+//{{6-1}} 透過關鍵字搜索得到的API匯入CREATEITEMS函數中
+let initiator = 0;
+
+function ShowData(){
+    initiator = 0;
+    section = page_list.length;
+    if(section){
+        for(let attr of page_list){
+            let attractionBox = CreateItems(attr);
+            Main.appendChild(attractionBox);
+        }
+    }
+    else if(!(Main.firstChild)){
+        let message = document.createElement('h1');
+        message.textContent = "未找到符合關鍵字的景點";
+        Main.appendChild(message);
+    }
+}
+//6. 檢查是否所有單一頁面資料都已經成功LOAD並且決定是否要LOAD下一頁的資料
+let load_next = false;
+function CheckPoint(){
+    initiator++;
+    if(initiator === section && nextPage !== null){
+        console.log(initiator,"attractions are already loaded")
+        load_next = true;
+    }
+}
+
+// 1.呼叫call LoadData() keyword預設為null
+LoadData(); 
+
+//7. infinite scroll設定
+//若(( window視窗本身的高度+cursor垂直方向的scroll >=  視窗body下緣-700pixel )) 且本業資料已經下載完成
+//則進入LoadData() 此時若無keyword預設，keywoed仍然為null且nextPage更新至下一頁
+//進入GetData(nextPage,keyword=null)
+
+if(nextPage !== null){
+    window.addEventListener('scroll',()=>{
+        if((window.innerHeight + window.scrollY) >= (document.body.getBoundingClientRect().bottom - 700) && load_next){
+            LoadData(keyword);
+            load_next = false;
+        }
+    })
+}
+
+// 關鍵字設定流程
+// 取得INDEX.HTML關鍵字ID
+const SearchForm= document.getElementById('SearchForm');
+const SearchKeyword = document.getElementById('SearchKey');
+//1-1.將前端關鍵字表單送出送出，先拒絕預設跳出整理
+//並且呼叫RemoveAll將屏幕更新
+//3-1 KEYWORD更新，並且將nextPage更新成預設=0
+//並且將INPUT中的內容匯入至LoadData(keyword)
+
+SearchForm.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    RemoveAll();
+    nextPage = 0;
+    keyword = SearchKeyword.value;
+    LoadData(keyword);    
+})
+//2-1 將屏幕上內容去除
+function RemoveAll(){
+    while(Main.firstChild){
+        Main.removeChild(Main.lastChild);
+    }
+}
+
+
+
+
+
+
+
+
